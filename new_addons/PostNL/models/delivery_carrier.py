7c5b5405a0ec9c12b1146c062ece2524fbcef8ef
MayGhonimy <mmohamed@unidistributors.com>
2022-03-01 11:01:02 +0200

    adding the customs to the shipping api  body for global shipping only

--- new_addons/PostNL/models/delivery_carrier.py
+++ new_addons/PostNL/models/delivery_carrier.py
@@ -15,14 +15,15 @@ class DeliveryCarrier(models.Model):
     delivery_type = fields.Selection(
         selection_add=[("post_nl", "PostNL")]
     )
-    api_key=fields.Char('PostNL Api Key')
+    api_key = fields.Char('PostNL Api Key')
 
     postnl_customer_code = fields.Char("Company Code")
-    postnl_customer_number=fields.Char("Company Number")
-    postnl_default_product_code=fields.Selection([('3085', '3085: Dutch domestic products - Standard shipping - Guaranteed Morning delivery'),
+    postnl_customer_number = fields.Char("Company Number")
+    postnl_default_product_code = fields.Selection([('3085', '3085: Dutch domestic products - Standard shipping - Guaranteed Morning delivery'),
                                                 ('4945', '4945: Global shipping')
                                                 #@ToDo add the rest of shipping types 
                                                 ],default='3085',string="Default Product Code Delivery")
+    postnl_gloable_license_nr = fields.Char("Global Shipping License")
                         
 
 
@@ -32,19 +33,54 @@ class DeliveryCarrier(models.Model):
         Might need to add more paramters in the future """
 
         return self.postnl_default_product_code
+    
+    
+    def _prepare_shipments_addresses_data(self,picking):
+        vals = {}
+        vals.update({"AddressType": "01",
+                 "City":picking.partner_id.city or ' ',
+                 "CompanyName":picking.partner_id.name or ' ',
+                 "Countrycode":picking.partner_id.country_id.code or ' ',
+                 "HouseNr":picking.partner_id.street2 or ' ',
+                 "Street":picking.partner_id.street or ' ',
+                 "Zipcode": picking.partner_id.zip or ''})
+        return vals
+
+    def _prepare_shipments_contacts_data(self,picking):
+        vals={}
+        vals.update({"ContactType": "01",
+                 "Email":picking.partner_id.email or ' ',
+                 "SMSNr":picking.partner_id.phone or ' '})
+        return vals
+
+    def _prepare_customs(self,picking):
+        """@TODO 1- add a function to switch all values to EUR (POSTNL only accept EURO or USS)
+            2- add a weight converter for diffrent types of UOM"""
+        vals = {}
+        _logger.info('getting customs for gloabl shippings only')
+        vals.update({"Content":[{"CountryOfOrigin": picking.company_id.country_id.code,
+                                    "Description": line.product_id.name,
+                                    "Quantity": line.product_uom_qty,
+                                    "Value": abs(line.value),
+                                    "Weight": line.weight} for line in picking.move_lines],
+                                "Currency": "EUR",
+                                "HandleAsNonDeliverable": "false",
+                                "License": "true",
+                                "LicenseNr":self.postnl_gloable_license_nr or ' ',
+                                "ShipmentType": "Commercial Goods"})
+        _logger.info(vals)
+        return vals
 
     def _prepare_customer_data(self,picking):
-        vals={}
-        vals.update({"Address":
-                {"AddressType": "02",
-                 "City":picking.company_id.city or ' ',
-                 "CompanyName":picking.company_id.name or ' ',
-                 "Countrycode":picking.company_id.country_id.code or ' ',
-                 "HouseNr":picking.company_id.street2 or ' ',
-                 "Street":picking.company_id.street or ' ',
-                 "Zipcode": picking.company_id.zip or ''
+        vals = {}
+        vals.update({"Address":{"AddressType": "02",
+                                 "City":picking.company_id.city or ' ',
+                                 "CompanyName":picking.company_id.name or ' ',
+                                 "Countrycode":picking.company_id.country_id.code or ' ',
+                                 "HouseNr":picking.company_id.street2 or ' ',
+                                 "Street":picking.company_id.street or ' ',
+                                 "Zipcode": picking.company_id.zip or ''},
 
-                },
                 "CollectionLocation" : picking.company_id.zip or '',
                 "ContactPerson": picking.company_id.name or '',
                 "CustomerCode": picking.carrier_id.postnl_customer_code or '',
@@ -54,77 +90,38 @@ class DeliveryCarrier(models.Model):
                 })
         return vals
 
-
-    def _prepare_shipments_addresses_data(self,picking):
-        vals={}
-        vals.update(
-                {"AddressType": "01",
-                 "City":picking.partner_id.city or ' ',
-                 "CompanyName":picking.partner_id.name or ' ',
-                 "Countrycode":picking.partner_id.country_id.code or ' ',
-                 "HouseNr":picking.partner_id.street2 or ' ',
-                 "Street":picking.partner_id.street or ' ',
-                 "Zipcode": picking.partner_id.zip or ''
-
-                })
-        return vals
-
-    def _prepare_shipments_contacts_data(self,picking):
-        vals={}
-        vals.update(
-                {"ContactType": "01",
-                 "Email":picking.partner_id.email or ' ',
-                 "SMSNr":picking.partner_id.phone or ' '
-                })
-        return vals
-
-            
+    def _prepare_shipments_data(self,picking):
+        vals = {}
+        vals.update({ "Addresses" : [self._prepare_shipments_addresses_data(picking)],                    
+                      "Contacts" : [self._prepare_shipments_contacts_data(picking)],
+                      "Dimension": {"Weight": "4300"},
+                      "ProductCodeDelivery": self.get_product_code()
+                    })
+       
+        if self.get_product_code() == '3085':
+            # customs ar required in globale shipping only and should't exist in other types
+            vals["Customs"] = self._prepare_customs(picking)
+        return vals       
 
 
     def ship(self,pickings):
+        _logger.info('starting the ship PostNL Porcess')
         
         current_date = time.strftime('%d-%m-%Y %H:%M:%S')
-        data ={
+        
+        data = {
             "Customer": self._prepare_customer_data(picking),
-            "Message": {
-                "MessageID": "01",
-                "MessageTimeStamp": current_date,
-                "Printertype": "GraphicFile|PDF"},
-            "Shipments": [
-                {
-                    "Addresses" : [self._prepare_shipments_addresses_data(picking)],                    
-                    "Contacts" : [self._prepare_shipments_contacts_data(picking)],
-                    "Customs" : {
-                        "Content": [{
-                                "CountryOfOrigin": "NL",
-                                "Description": "Powdered milk",
-                                "HSTariffNr": "19019091",
-                                "Quantity": "2",
-                                "Value": "20.00",
-                                "Weight": "4300"
-                            }],
-                        "Currency": "EUR",
-                        "HandleAsNonDeliverable": "false",
-                        "Invoice": "true",
-                        "InvoiceNr": "22334455",
-                        "ShipmentType": "Commercial Goods"
-                    },
-                    "Dimension": {
-                        "Weight": "4300"
-                    },
-                    "ProductCodeDelivery": self.get_product_code()
-                }
-            ]
+            "Message": {"MessageID": "01",
+                        "MessageTimeStamp": current_date,
+                        "Printertype": "GraphicFile|PDF"},
+            "Shipments": [self._prepare_shipments_data(picking)]
         }
-        print(data)
-        response=PostNLRequets(self.api_key).ship(data)
         
-        print(response.content)
+        _logger.info('JSON body to the shipping api: %s' % ((data)))
+        response = PostNLRequets(self.api_key).ship(data)        
         _logger.info('PostNL Response : %s' % (response.content))
-        print({response.content})
         
-
-
+        
 
 
         
