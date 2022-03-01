c21247950c1441a26e485c5d246b71348888b76c
MayGhonimy <mmohamed@unidistributors.com>
2022-03-01 07:09:29 +0200

    feeding json for post request to POSTNL shipping

--- new_addons/PostNL/models/delivery_carrier.py
+++ new_addons/PostNL/models/delivery_carrier.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models
+from odoo import fields, models,_
 from .postNL_requests import PostNLRequets
 import logging
 _logger = logging.getLogger(__name__)
-
+from odoo.exceptions import UserError
+import json
+import time
 
 
 class DeliveryCarrier(models.Model):
@@ -13,31 +15,47 @@ class DeliveryCarrier(models.Model):
     delivery_type = fields.Selection(
         selection_add=[("post_nl", "PostNL")]
     )
-    api_key
+    api_key=fields.Char('PostNL Api Key')
 
-    
-    def ship(self):
-        data ={
-            "Customer": {
-                "Address": {
-                    "AddressType": "02",
-                    "City": "Hoofddorp",
-                    "CompanyName": "PostNL",
-                    "Countrycode": "NL",
-                    "HouseNr": "42",
-                    "Street": "Siriusdreef",
-                    "Zipcode": "2132WT"
+    postnl_customer_code = fields.Char("Company Code")
+    postnl_customer_number=fields.Char("Company Number")
+
+
+    def _prepare_customer_data(self,picking):
+        vals={}
+        vals.update({"Address":
+                {"AddressType": "02",
+                 "City":picking.partner_id.city or ' ',
+                 "CompanyName":picking.partner_id.name or ' ',
+                 "Countrycode":picking.partner_id.country_id.code or ' ',
+                 "HouseNr":picking.partner_id.street2 or ' ',
+                 "Street":picking.partner_id.street or ' ',
+                 "Zipcode": picking.partner_id.zip or ''
+
                 },
-                "CollectionLocation": "1234506",
-                "ContactPerson": "Janssen",
-                "CustomerCode": "DEVC",
-                "CustomerNumber": "11223344",
-                "Email": "email@company.com",
-                "Name": "Janssen"
-            },
+                "CollectionLocation" : picking.company_id.zip or '',
+                "ContactPerson": picking.company_id.name or '',
+                "CustomerCode": picking.carrier_id.postnl_customer_code or '',
+                "CustomerNumber":picking.carrier_id.postnl_customer_number or '' ,
+                "Email":picking.company_id.email,
+                "Name":picking.create_uid.name
+                })
+        return vals
+
+
+    def _prepare_shipments_data(self,picking):
+        
+            
+
+
+    def ship(self,pickings):
+        
+        current_date = time.strftime('%d-%m-%Y %H:%M:%S')
+        data ={
+            "Customer": self._prepare_customer(picking),
             "Message": {
-                "MessageID": "{{$guid}}",
-                "MessageTimeStamp": "{{dateTime}}",
+                "MessageID": "01",
+                "MessageTimeStamp": current_date,
                 "Printertype": "GraphicFile|PDF"
             },
             "Shipments": [
@@ -87,8 +105,13 @@ class DeliveryCarrier(models.Model):
                 }
             ]
         }
-        request=PostNLRequets(data)
-        _logger.info(" ship request response : %s" % (request))
+        print(data)
+        response=PostNLRequets(self.api_key).ship(data)
+        
+        print(response.content)
+        _logger.info('PostNL Response : %s' % (response.content))
+        print({response.content})
+        
 
 
 
