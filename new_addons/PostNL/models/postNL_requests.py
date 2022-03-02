c5d6bc491676ac408a1eb63ee9d8d467fe8af25a
MayGhonimy <mmohamed@unidistributors.com>
2022-03-02 14:39:55 +0200

    add prod_enviroment option to switch between test link and production link from UI

--- new_addons/PostNL/models/postNL_requests.py
+++ new_addons/PostNL/models/postNL_requests.py
@@ -1,35 +1,37 @@
 # -*- coding: utf-8 -*-
 
 from odoo import _
-from odoo.exceptions import ValidationError,UserError
+from odoo.exceptions import ValidationError, UserError
 import requests
 import json
 
 
-
 class PostNLRequets:
-    def __init__(self, api_key=None):
+    def __init__(self, api_key=None,prod_environment=False):
         if api_key:
             self.api_key = api_key
         else:
-            self.api_key='1'
-        self.PostNL_URL = 'https://api-sandbox.postnl.nl/'
+            self.api_key = '1'
+        if prod_environment:
+            self.postNL_url = 'https://api.postnl.nl/shipment/'
+        else:
+            self.postNL_url = 'https://api-sandbox.postnl.nl/'
 
     def __str__(self):
         return f'PostNLRequets with key {self.api_key}'
 
-    def ship(self,body):
+    def ship(self, body):
         try:
-            body = json.dumps(body)
+            body = json.loads(json.dumps(body))
         except Exception as e:
-            raise  UserError( _('Data Error: [%s]'% (e) ))        
+            raise UserError(_('Data Error: [%s]'% (e)))
         hdr = {'content-type': 'application/json',
                'apikey': self.api_key}
-        url = f'{self.PostNL_URL}v1/shipment'
+        url = f'{self.postNL_url}v1/shipment'
         response = requests.post(url, data=body, headers=hdr)
-        response_json_string=response.content.decode('utf-8')
+        response_json_string = response.content.decode('utf-8')
         response_json_object = json.loads(str(response_json_string))
         if response.status_code != 200:
-            raise ValidationError(_(f"Error<{response.status_code}>: {response_json_object['fault']['faultstring']}"))    
+            raise ValidationError(_(f"Error<{response.status_code}>: {response_json_object['fault']['faultstring']}"))
 
         return dict(response_json_object)
